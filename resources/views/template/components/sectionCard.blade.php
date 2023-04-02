c539ef21dbb3f727096be59d0124a3371508b341
vuduc231 <vuduc23195@gmail.com>
2023-04-02 14:43:53 +0700

    fixlayout controller

--- resources/views/template/components/sectionCard.blade.php
+++ resources/views/template/components/sectionCard.blade.php
@@ -1,19 +1,15 @@
 <div class="mainSection_card">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="text-nowrap">Đơn vị: </div>
         </div>
-<<<<<<< HEAD
-        <div class="col-md-9"><strong class="text-nowrap">{{ session('user')['departement']['name'] ?? 'Chưa có đơn vị' }}</strong>
-=======
         <div class="col-md-8"><strong class="text-nowrap">{{Session::get('department_name')}}</strong>
->>>>>>> 23d5c8b9ad47f9a3722600519de4b9d2a31fd337
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="text-nowrap">Trưởng đơn vị: </div>
         </div>
-        <div class="col-md-9"><strong class="text-nowrap">{{ Session::get('user')['name'] }} -
-                {{ Session::get('user')['code'] ?? 'Chưa có mã' }}</strong></div>
+        <div class="col-md-8"><strong class="text-nowrap">{{ Session::get('user')['name'] }} -
+                {{ Session::get('user')['code'] ?? '' }}</strong></div>
     </div>
 
 </div>
