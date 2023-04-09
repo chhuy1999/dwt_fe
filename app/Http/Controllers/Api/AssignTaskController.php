3895ad5a31ef7830adaeff645b8145626e610521
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-04-09 19:13:28 +0700

    commit to pull

--- app/Http/Controllers/Api/AssignTaskController.php
+++ app/Http/Controllers/Api/AssignTaskController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class AssignTaskController extends Controller
 {
     //
-    private $dwtService;
+    private DwtServices $dwtService;
 
     public function __construct()
     {
