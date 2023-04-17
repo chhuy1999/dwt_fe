5ddb9b73a06aca4ea6d61d1c003f27f7cdb8c0c6
vuduc231 <vuduc23195@gmail.com>
2023-04-17 16:18:38 +0700

    fix page profile

--- resources/views/page/information/profile.blade.php
+++ resources/views/page/information/profile.blade.php
@@ -37,7 +37,7 @@
                     </div>
                     <div class="information_wrapper bg-white">
                         <div class="row">
-                            <div class="col-6 col-md-6">
+                            <div class="col-4 col-md-4">
                                 <div class="card" style="height: 100%;">
                                     <div class="card-body">
                                         <div class="information_image-upload">
@@ -50,15 +50,47 @@
                                             </label>
                                         </div>
                                         <div class="information_signature-wrapper">
-                                            <button role="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#taoChuKy">Tạo chữ ký</button>
+                                            <button role="button" class="btn btn-danger" data-bs-toggle="modal"
+                                                data-bs-target="#taoChuKy">Tạo chữ ký</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-6 col-md-6">
+                            <div class="col-8 col-md-8">
                                 <div class="card" style="height: 100%;">
                                     <div class="card-body">
-                                        2
+                                        <div class="row">
+                                            <div class="mb-3 col-6">
+                                                <input type="text" readonly value="Đặng Vũ Lam Mai" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-3">
+                                                <input type="text" readonly value="MTT239" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-3">
+                                                <input type="text" readonly value="27/04/1999" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-7">
+                                                <input type="text" readonly value="219 Trung Kính" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-5">
+                                                <input type="text" readonly value="09123465789" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-4">
+                                                <input type="text" readonly value="bmtkt1@dopperherz.vn" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-4">
+                                                <input type="text" readonly value="Phòng Marketing" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-4">
+                                                <input type="text" readonly value="Trợ lý Marketing" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-6">
+                                                <input type="text" readonly value="bmtkt1@dopperherz.vn" class="form-control">
+                                            </div>
+                                            <div class="mb-3 col-6">
+                                                <input type="text" readonly value="**********" class="form-control">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -119,153 +151,36 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="create_user-wrapper">
-                        <div class="create_user-title mb-2">Thông tin cá nhân</div>
-                        <div class="row">
-                            <div class="col-sm-2">
-                                <div class="image-upload">
-                                    <input type="file" name="" id="logo" onchange="fileValue(this)">
-                                    <label for="logo" class="upload-field" id="file-label">
-                                        <div class="file-thumbnail">
-                                            <img id="image-preview" src="{{ asset('assets/img/preview.jpg') }}"
-                                                alt="">
-                                        </div>
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="col-sm-5">
-                                <div class="d-flex align-items-center mb-2">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Họ và tên <span class="text-danger">*</span></div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" type="text" value="Đặng Vũ Lam Mai">
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center mb-2">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Ngày sinh<span class="text-danger">*</span></div>
-                                    </div>
-                                    <div class="col-sm-8 position-relative">
-                                        <input id="suaCreateUser" value="<?php echo date('d/m/Y'); ?>" class="form-control"
-                                            type="text">
-                                        <i class="bi bi-calendar-plus style_pickdate"></i>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Số điện thoại<span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class=" col-sm-8">
-                                        <input class="form-control" type="text" value="0123456789">
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col-sm-5">
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Giới tính <span class="text-danger">*</span></div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                id="inlineRadio1" value="option1">
-                                            <label class="form-check-label" for="inlineRadio1">Nam</label>
-                                            <div class="form-check form-check-inline">
-                                                <input class="form-check-input" checked type="radio"
-                                                    name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                                <label class="form-check-label" for="inlineRadio2">Nữ</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Email<span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" type="text" value="vuha@gmail.com">
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Địa chỉ<span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" type="text" value="219 trung kính">
-                                    </div>
-                                </div>
-
-                            </div>
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <input type="text" readonly value="Đặng Vũ Lam Mai" class="form-control">
                         </div>
-                    </div>
-
-                    <div class="create_user-wrapper">
-                        <div class="create_user-title mb-2">Thông tin công việc</div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Mã nhân viên <span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" type="text" value="MTT123">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6">
-                                <div class="d-flex align-items-center">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Email liên kết<span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" type="text" value="bmtkt1@dopperherz.vn">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Đơn vị công tác <span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <select class="selectpicker">
-                                            <option checked>CTCP Mastertran</option>
-                                            <option>Doppelherz</option>
-                                            <option>CTCP Thái Bình Hưng Thịnh</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6">
-                                <div class="d-flex align-items-center mb-3">
-                                    <div class="d-flex col-sm-4">
-                                        <div class="modal_body-title">Vị trí/Chức danh <span class="text-danger">*</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <select id="onchangeViTriCongViecfix" class="selectpicker">
-                                            <option>Quản lý phòng</option>
-                                            <option>Quản lý sàn TMĐT</option>
-                                            <option>Content Website</option>
-                                            <option>Content SEO</option>
-                                            <option>Google Ads</option>
-                                            <option>Content Facebook</option>
-                                            <option value="themViTriCongViecfix">Thêm vị trí mới</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
+                        <div class="mb-3 col-3">
+                            <input type="text" readonly value="MTT239" class="form-control">
+                        </div>
+                        <div class="mb-3 col-3">
+                            <input type="text" readonly value="27/04/1999" class="form-control">
+                        </div>
+                        <div class="mb-3 col-7">
+                            <input type="text" readonly value="219 Trung Kính" class="form-control">
+                        </div>
+                        <div class="mb-3 col-5">
+                            <input type="text" readonly value="09123465789" class="form-control">
+                        </div>
+                        <div class="mb-3 col-4">
+                            <input type="text" readonly value="bmtkt1@dopperherz.vn" class="form-control">
+                        </div>
+                        <div class="mb-3 col-4">
+                            <input type="text" readonly value="Phòng Marketing" class="form-control">
+                        </div>
+                        <div class="mb-3 col-4">
+                            <input type="text" readonly value="Trợ lý Marketing" class="form-control">
+                        </div>
+                        <div class="mb-3 col-6">
+                            <input type="text" readonly value="bmtkt1@dopperherz.vn" class="form-control">
+                        </div>
+                        <div class="mb-3 col-6">
+                            <input type="text" readonly value="**********" class="form-control">
                         </div>
                     </div>
                 </div>
@@ -288,9 +203,9 @@
                 <div class="modal-body">
                     <div class="row">
                         <div class="col-12">
-                            <div class="information_signature" style="height: 200px"; width: 200px;>
-                                <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
-                            </div>
+                                <div class="information_signature" style="height: 200px; width: 200px;">
+                                    <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
+                                </div>
                             <button id="save-png">Save as PNG</button>
                             <button id="save-jpeg">Save as JPEG</button>
                             <button id="save-svg">Save as SVG</button>
@@ -317,75 +232,26 @@
 
 
     <script>
-        document.addEventListener("DOMContentLoaded", event => {
-            var canvas = document.getElementById('signature-pad');
-
-// Adjust canvas coordinate space taking into account pixel ratio,
-// to make it look crisp on mobile devices.
-// This also causes canvas to be cleared.
-function resizeCanvas() {
-    // When zoomed out to less than 100%, for some very strange reason,
-    // some browsers report devicePixelRatio as less than 1
-    // and only part of the canvas is cleared then.
-    var ratio = Math.max(window.devicePixelRatio || 1, 1);
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
-}
-
-window.onresize = resizeCanvas;
-resizeCanvas();
-
-var signaturePad = new SignaturePad(canvas, {
+        $('#myModal').on('shown.bs.modal', function () {
+  var canvas = document.getElementById('signature-pad');
+  var signaturePad = new SignaturePad(canvas, {
     backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
-});
+  });
 
-document.getElementById('save-png').addEventListener('click', function() {
-    if (signaturePad.isEmpty()) {
-        return alert("Please provide a signature first.");
-    }
-
-    var data = signaturePad.toDataURL('image/png');
-    console.log(data);
-    window.open(data);
-});
-
-document.getElementById('save-jpeg').addEventListener('click', function() {
-    if (signaturePad.isEmpty()) {
-        return alert("Please provide a signature first.");
-    }
-
-    var data = signaturePad.toDataURL('image/jpeg');
-    console.log(data);
-    window.open(data);
-});
-
-document.getElementById('save-svg').addEventListener('click', function() {
-    if (signaturePad.isEmpty()) {
-        return alert("Please provide a signature first.");
-    }
-
-    var data = signaturePad.toDataURL('image/svg+xml');
-    console.log(data);
-    console.log(atob(data.split(',')[1]));
-    window.open(data);
-});
-
-document.getElementById('clear').addEventListener('click', function() {
+  document.getElementById('clear').addEventListener('click', function() {
     signaturePad.clear();
-});
+  });
 
-document.getElementById('draw').addEventListener('click', function() {
+  document.getElementById('draw').addEventListener('click', function() {
     var ctx = canvas.getContext('2d');
-    console.log(ctx.globalCompositeOperation);
     ctx.globalCompositeOperation = 'source-over'; // default value
-});
+  });
 
-document.getElementById('erase').addEventListener('click', function() {
+  document.getElementById('erase').addEventListener('click', function() {
     var ctx = canvas.getContext('2d');
     ctx.globalCompositeOperation = 'destination-out';
-});  
-        })
-        
+  });
+});
+
     </script>
 @endsection
